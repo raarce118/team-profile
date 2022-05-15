9cbc862d72c3b3becea9b7185c0611dfdf457c38
Rich Arce <raarce118@gmail.com>
2022-05-15 15:59:17 -0700

    added employee test and script

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,38 @@
+const Employee = require('../lib/Employee.js');
+jest.mock('../lib/Employee.js');
+
+
+// copied and edited from module 10 tests.js
+test('creates an Employee object', () => {
+  const Employee = new Employee('Rich', 8, 'r.arce118@hotmail.com', 'manager');
+
+  expect(Employee.name).toEqual(expect.any(String));
+  expect(Employee.id).toEqual(expect.any(Number));
+  expect(Employee.email).toEqual(expect.any(String));
+  expect(Employee.Role).toEqual(expect.any(String));
+
+});
+
+test('gets name of employee', () => {
+    const Employee = new Employee('Rich', 8, 'r.arce118@hotmail.com', 'manager');
+    expect(Employee.getName()).toEqual(expect.any(String));
+  });
+
+  
+test('gets ID of employee', () => {
+    const Employee = new Employee('Rich', 8, 'r.arce118@hotmail.com', 'manager');
+    expect(Employee.getId()).toEqual(expect.any(Number));
+  });
+
+test('gets email of employee', () => {
+    const Employee = new Employee('Rich', 8, 'r.arce118@hotmail.com', 'manager');
+    expect(Employee.getEmail()).toEqual(expect.any(String));
+  });
+
+test('gets Role of employee', () => {
+    const Employee = new Employee('Rich', 8, 'r.arce118@hotmail.com', 'manager');
+    expect(Employee.getRole()).toEqual(expect.any(String));
+  });
+
+
+  
